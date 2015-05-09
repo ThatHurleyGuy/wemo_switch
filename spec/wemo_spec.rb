8a994dc52033d7b14185cf513aee07c80ba03c24
hurley <sean.hurley6@gmail.com>
2015-05-09 13:37:27 -0500

    Add a few simple smoke tests

--- spec/wemo_spec.rb
+++ spec/wemo_spec.rb
@@ -1,7 +1,28 @@
 require 'spec_helper'
 
 describe Wemo do
-  it "loads" do
-    expect(1).to eq 1
+  let(:wemo) { Wemo.new("192.168.1.150") }
+
+  it "can get a light's status" do
+    state = wemo.get_state
+
+    expect([true, false]).to include(state)
+  end
+
+  it "can change the state" do
+    expect do
+      wemo.toggle_outlet
+    end.to change { wemo.get_state }
+  end
+
+  it "will try multiple ports" do
+    allow_any_instance_of(Savon::Client).to receive(:call).and_raise(Net::ReadTimeout)
+    expect_any_instance_of(Wemo).to receive(:_outlet_endpoint).with(49151).and_return("http://192.168.1.150:49151")
+    expect_any_instance_of(Wemo).to receive(:_outlet_endpoint).with(49153).and_return("http://192.168.1.150:49153")
+    expect_any_instance_of(Wemo).to receive(:_outlet_endpoint).with(49154).and_return("http://192.168.1.150:49154")
+
+    expect do
+      wemo.toggle_outlet
+    end.to  raise_error
   end
 end
--- wemo.gemspec
+++ wemo.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake", "~> 10.4"
   spec.add_development_dependency "rspec", "~> 3.2"
+  spec.add_development_dependency "rspec-mocks", "~> 3.2"
   spec.add_dependency "savon", "~> 2.11"
 
   spec.required_ruby_version = ">= 2.0.0"
